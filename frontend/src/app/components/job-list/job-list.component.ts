31ae822fa9e2cce60b304009813da51eb4130fb2
rawoof47 <i.rawoof4@gmail.com>
2025-04-21 17:01:43 +0530

    Added job model and updated job list component

--- frontend/src/app/components/job-list/job-list.component.ts
+++ frontend/src/app/components/job-list/job-list.component.ts
@@ -1,4 +1,7 @@
+// src/app/components/job-list/job-list.component.ts
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Job } from '../models/job.model';  // This assumes 'job-list' is in 'components' folder
 
 @Component({
   selector: 'app-job-list',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-list.component.scss']
 })
 export class JobListComponent implements OnInit {
+  jobs: Job[] = []; // Type the jobs array using the Job interface
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http.get<Job[]>('http://localhost:3000/api/jobs').subscribe(
+      (data) => {
+        this.jobs = data;
+      },
+      (error) => {
+        console.error('Error fetching job data', error);
+      }
+    );
   }
 
+  applyJob(jobId: number): void {
+    alert(`You have applied for job with ID: ${jobId}`);
+  }
 }
---
+++ frontend/src/app/components/models/job.model.ts
@@ -0,0 +1,11 @@
+// src/app/models/job.model.ts
+
+export interface Job {
+    id: number;
+    company: string;
+    role: string;
+    salary: number;
+    type: string;
+    postedDate: string;
+  }
+  
